9c1bd4c7395b4b94db8c92bac5709f628ab1ebc4
Joao Paulo <joaoolintogomes159@gmail.com>
2026-06-14 11:46:06 -0300

    feat: add initial contract and installment models

--- src/domain/entities/Contract.java
+++ src/domain/entities/Contract.java
@@ -1,6 +1,8 @@
 package domain.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import domain.exception.DomainException;
 
@@ -10,42 +12,43 @@ public class Contract {
 	private String number;
 	private LocalDate date;
 	private double totalValue;
+	private List<Installment> installments = new ArrayList<>();
 
 	// Construtor
-	public Contract(String number, LocalDate date, double totalValue) {
+	public Contract(String number, LocalDate date, Installment installment) {
 
-		//Aplicando terminadas regras de negocio
-		//O numero do pedido deve conter somente digitos numericos(4 digitos especificamente)
-		if(!number.trim().matches("^[\\d{4}$")) {
+		// Aplicando terminadas regras de negocio
+		// O numero do pedido deve conter somente digitos numericos(4 digitos
+		// especificamente)
+		if (!number.trim().matches("^[\\d{4}$")) {
 			throw new DomainException("Domain error: The contract number can only contain 4 numeric digits.");
 		}
-		
-		//O valor total do contrato não pode ser inferior a zero
-		if(totalValue < 0.0) {
+
+		// O valor total do contrato não pode ser inferior a zero
+		if (totalValue < 0.0) {
 			throw new DomainException("Domain error: the total value cannot be less than zero");
 		}
-		
-		//A data de criação do contrato não pode ser posterior a atual
-		if(date.isAfter(LocalDate.now())) {
-			throw new DomainException("Domain error: The date of signature of this contract cannot be earlier than the current date.");
+
+		// A data de criação do contrato não pode ser posterior a atual
+		if (date.isAfter(LocalDate.now())) {
+			throw new DomainException(
+					"Domain error: The date of signature of this contract cannot be earlier than the current date.");
 		}
 		this.number = number;
 		this.date = date;
-		this.totalValue = totalValue;
-
 	}
 
 	// getters e setters
-	//Aplicamos as validações
+	// Aplicamos as validações
 	public String getNumber() {
 		return number;
 	}
 
 	public void setNumber(String number) {
-		if(!number.trim().matches("^[\\d{4}$")) {
+		if (!number.trim().matches("^[\\d{4}$")) {
 			throw new DomainException("Domain error: The contract number can only contain 4 numeric digits.");
 		}
-		
+
 		this.number = number;
 	}
 
@@ -54,24 +57,24 @@ public class Contract {
 	}
 
 	public void setDate(LocalDate date) {
-		if(date.isAfter(LocalDate.now())) {
-			throw new DomainException("Domain error: The date of signature of this contract cannot be earlier than the current date.");
+		if (date.isAfter(LocalDate.now())) {
+			throw new DomainException(
+					"Domain error: The date of signature of this contract cannot be earlier than the current date.");
 		}
-		
-		
+
 		this.date = date;
 	}
 
 	public double getTotalValue() {
 		return totalValue;
 	}
-	
+
 	public void setTotalValue(double totalValue) {
 
-		if(totalValue < 0.0) {
+		if (totalValue < 0.0) {
 			throw new DomainException("Domain error: the total value cannot be less than zero");
 		}
-		
+
 		this.totalValue = totalValue;
 	}
 
--- src/domain/entities/Installment.java
+++ src/domain/entities/Installment.java
@@ -1,35 +1,45 @@
 package domain.entities;
 
 import java.time.LocalDate;
+import domain.exception.DomainException;
 
 //Classe que tem como função representar uma parcela, possuindo  valor e data.
 public class Installment {
 
 	private LocalDate dueDate;
 	private double amount;
-	
-	//Construtor onde aplicamos a injeção de dependencia
+
+	// Construtor onde aplicamos a injeção de dependencia
 	public Installment(LocalDate dueDate, double amount) {
-		
+
+		if (amount < 0.0) {
+			throw new DomainException("Domain error: ");
+		}
+
 		this.dueDate = dueDate;
 		this.amount = amount;
-		
+
 	}
-	
+
 	public LocalDate getDueDate() {
 		return dueDate;
 	}
-	
+
 	public void setDueDate(LocalDate dueDate) {
 		this.dueDate = dueDate;
 	}
-	
+
 	public double getAmount() {
+
 		return amount;
 	}
-	
+
 	public void setAmount(double amount) {
+		if (amount < 0.0) {
+			throw new DomainException("Domain error: ");
+		}
+
 		this.amount = amount;
 	}
-	
+
 }
