268f3740d3d301f0669c20e111b4afce6482b75e
Joao Paulo <joaoolintogomes159@gmail.com>
2026-06-16 21:42:35 -0300

    adds comments to the ContractService class

--- src/domain/service/ContractService.java
+++ src/domain/service/ContractService.java
@@ -10,15 +10,23 @@ public class ContractService {
 	
 	//Metodo para processar o contrato, gerando as parcelas do mesmo
 	public void processContract(Contract contract, int months) {
+		
 		//Aqui instanciamos a classe responsavel pelo pagament online
 		OnlinePaymentService onlinePay =  new PayPalService();
 		
+		//Variavel que representa o valor da parcela base
 		double installmentValue = 0.0;
 		for(int i = 0; i < months; i++) {
 			
+			//Dividimos o valor pela quantidade de parcelas/meses
+			installmentValue = contract.getTotalValue() / months;
+			//chamada ao metodo responsavel por calcular o juros simples
 			installmentValue = onlinePay.interest(contract.getTotalValue(), i);
+			//Adiciona a taxa de 2%
 			installmentValue += onlinePay.paymentFee(installmentValue);
+			//adiciona a quantidade de mes com base no valor de i 
 			LocalDate dueDate = contract.getDate().plusMonths(i);
+			//Instancia e adiciona a lista de parcelas
 			contract.addInstallment(new Installment(dueDate,installmentValue));
 		}
 	}
--- src/domain/service/PayPalService.java
+++ src/domain/service/PayPalService.java
@@ -5,19 +5,18 @@ public class PayPalService implements OnlinePaymentService {
 	@Override
 	//Metodo responsavel por calcular a taxa de 2% do pay Pal
 	public double paymentFee(double amount) {
-		double fee = amount * 0.02;
-		return  fee;
+		//retorna o valor da taxa da parcela
+		return  amount * 0.02;
 	
 	}
 
 	@Override
 	//Metodo que calcula o juros simples do valor
 	public double interest(double amount, int months) {
-		double installmentValue = amount / months;
-		installmentValue += installmentValue * 0.01 * months; 
+		
+		//Calculamos os juros simples com base n valor de 1%
+		double installmentValue =  amount * 0.01 * months; 
+		
 		return installmentValue;
 	}
-	
-	
-
 }
